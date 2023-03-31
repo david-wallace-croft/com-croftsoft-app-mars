6a6f179358cca82c68da9e63587a9ab6f9b9b7dc
David Wallace Croft <david@CroftSoft.com>
2023-03-30 19:41:32 -0500

    tank constants

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -5,13 +5,14 @@
 //! - Copyright: &copy; 2023 [`CroftSoft Inc`]
 //! - Author: [`David Wallace Croft`]
 //! - Created: 2023-03-11
-//! - Updated: 2023-03-29
+//! - Updated: 2023-03-30
 //!
 //! [`CroftSoft Inc`]: https://www.croftsoft.com/
 //! [`David Wallace Croft`]: https://www.croftsoft.com/people/david/
 // =============================================================================
 
 use crate::state::configuration::Configuration;
+use core::f64::consts::TAU;
 
 pub const BACKGROUND_FILL_STYLE: &str = "rgb(255, 152, 109)";
 pub const CONFIGURATION: Configuration = Configuration {
@@ -25,35 +26,19 @@ pub static INFO: &str = "CroftSoft Mars \
 pub const MILLIS_PER_SECOND: f64 = 1_000.;
 pub const OBSTACLE_FILL_STYLE: &str = "#40826D";
 pub const OBSTACLE_STROKE_STYLE: &str = "black";
-pub const OBSTACLE_JERK_MAGNITUDE_MAX: f64 = 100.0;
-pub const OBSTACLE_SPEED_MAX: f64 = 1_000.0;
+pub const OBSTACLE_JERK_MAGNITUDE_MAX: f64 = 100.;
+pub const OBSTACLE_SPEED_MAX: f64 = 1_000.;
 pub const OBSTACLE_Z: f64 = 0.2;
 pub const OVERLAY_FILL_STYLE: &str = "black";
 pub const OVERLAY_REFRESH_PERIOD_MILLIS: f64 = 1_000.;
+pub const TANK_AMMO_INITIAL: usize = TANK_DAMAGE_MAX as usize + 1;
+pub const TANK_AMMO_MAX: usize = 30;
+pub const TANK_BODY_ROTATION_SPEED_RADIANS_PER_SECOND: f64 = TAU / 6.;
+pub const TANK_DAMAGE_MAX: f64 = 2.;
+pub const TANK_RADIUS: f64 = 25.;
+pub const TANK_SPEED_METERS_PER_SECOND: f64 = 30.;
+pub const TANK_TURRET_ROTATION_SPEED_RADIANS_PER_SECOND: f64 = TAU / 2.;
 pub const TANK_Z: f64 = 1.;
 pub const TIME_DELTA: f64 = 1. / UPDATES_PER_SECOND;
 pub const UPDATE_PERIOD_MILLIS: f64 = MILLIS_PER_SECOND / UPDATES_PER_SECOND;
 pub const UPDATES_PER_SECOND: f64 = 60.;
-
-// TODO
-// 046         /** radians per second */
-// 047         private static final double  TURRET_ROTATION_SPEED
-// 048           = 2.0 * Math.PI / 2.0;
-// 049
-// 050         /** radians per second */
-// 051         private static final double  BODY_ROTATION_SPEED
-// 052           = 2.0 * Math.PI / 6.0;
-// 053
-// 054         /** meters per second */
-// 055         private static final double  TANK_SPEED = 30.0;
-// 056
-// 057         /** meters per second */
-// 058         private static final double  RADIUS = 25.0;
-// 059
-// 060         private static final int     MAX_AMMO = 30;
-// 061
-// 062         private static final double  DAMAGE_MAX = 2.0;
-// 063
-// 064         //
-// 065
-// 066         private static final int  INITIAL_AMMO = ( int ) DAMAGE_MAX + 1;
