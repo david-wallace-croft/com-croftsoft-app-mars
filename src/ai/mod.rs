610faa0e8f41ddbd06b2fa658584651ced2781a0
David Wallace Croft <david@CroftSoft.com>
2023-06-03 20:06:36 -0500

    AI module rust doc

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -1,3 +1,16 @@
+// =============================================================================
+//! - Artificial Intelligence (AI) module for CroftSoft Mars
+//!
+//! # Metadata
+//! - Copyright: &copy; 2023 [`CroftSoft Inc`]
+//! - Author: [`David Wallace Croft`]
+//! - Created: 2023-06-03
+//! - Updated: 2023-06-03
+//!
+//! [`CroftSoft Inc`]: https://www.croftsoft.com/
+//! [`David Wallace Croft`]: https://www.croftsoft.com/people/david/
+// =============================================================================
+
 pub mod state_space_node;
 pub mod tank_cartographer;
 pub mod tank_operator;
