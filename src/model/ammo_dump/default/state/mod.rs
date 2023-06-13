8cadc300f0f14dccf5288a8b1f06c9a8bd5cd431
David Wallace Croft <david@CroftSoft.com>
2023-06-13 07:27:58 -0500

    state data

--- src/model/ammo_dump/default/state/mod.rs
+++ src/model/ammo_dump/default/state/mod.rs
@@ -13,19 +13,19 @@
 
 use crate::constant::AMMO_DUMP_COOLING_TIME_SECONDS;
 
-pub struct ForCooling {
+pub struct CoolingData {
   cooling_time_elapsed_seconds: f64,
 }
 
-pub struct ForExploding;
+pub struct ExplodingData;
 
-pub struct ForNominal;
+pub struct NominalData;
 
 pub struct StateOperator<D> {
   data: D,
 }
 
-impl StateOperator<ForCooling> {
+impl StateOperator<CoolingData> {
   pub fn done_cooling(
     &mut self,
     time_delta: f64,
@@ -36,39 +36,39 @@ impl StateOperator<ForCooling> {
 
   pub fn to_nominal(&self) -> State {
     State::Nominal(StateOperator {
-      data: ForNominal {},
+      data: NominalData {},
     })
   }
 }
 
-impl StateOperator<ForExploding> {
+impl StateOperator<ExplodingData> {
   pub fn to_cooling(&self) -> State {
     State::Cooling(StateOperator {
-      data: ForCooling {
+      data: CoolingData {
         cooling_time_elapsed_seconds: 0.,
       },
     })
   }
 }
 
-impl StateOperator<ForNominal> {
+impl StateOperator<NominalData> {
   pub fn to_exploding(&self) -> State {
     State::Exploding(StateOperator {
-      data: ForExploding {},
+      data: ExplodingData {},
     })
   }
 }
 
 pub enum State {
-  Cooling(StateOperator<ForCooling>),
-  Exploding(StateOperator<ForExploding>),
-  Nominal(StateOperator<ForNominal>),
+  Cooling(StateOperator<CoolingData>),
+  Exploding(StateOperator<ExplodingData>),
+  Nominal(StateOperator<NominalData>),
 }
 
 impl Default for State {
   fn default() -> Self {
     State::Nominal(StateOperator {
-      data: ForNominal {},
+      data: NominalData {},
     })
   }
 }
