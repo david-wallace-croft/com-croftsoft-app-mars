b39c029dff29bd42bbfaccaf1ec4a3ac133cb259
David Wallace Croft <david@croftsoft.com>
2023-06-18 08:46:18 -0500

    paint path

--- src/painter/tank/mod.rs
+++ src/painter/tank/mod.rs
@@ -11,9 +11,10 @@
 //! [`David Wallace Croft`]: https://www.croftsoft.com/people/david/
 // =============================================================================
 
+use crate::ai::state_space_node::StateSpaceNode;
 use crate::constant::{
   TANK_FILL_STYLE_BLUE, TANK_FILL_STYLE_RED, TANK_FILL_STYLE_SPARKING,
-  TANK_STROKE_STYLE,
+  TANK_STROKE_STYLE, TANK_RADIUS,
 };
 use crate::model::tank::{Color, Tank};
 use com_croftsoft_core::math::geom::circle::Circle;
@@ -146,8 +147,29 @@ impl TankPainter {
       context.rect(-5., -5., 10., 10.);
       context.fill();
     }
-    // end
+    // end tank
     context.restore();
+    // path
+    context.save();
+    let stroke_style = match tank.get_color() {
+      Color::RED => &self.fill_style_red,
+      Color::BLUE => &self.fill_style_blue,
+    };
+    context.set_stroke_style(stroke_style);
+    context.set_line_width(3.);
+    if let Some(tank_operator) = tank.get_tank_operator() {
+      let tank_operator = tank_operator.borrow();
+      let state_space_nodes: Vec<StateSpaceNode> = tank_operator.get_path();
+      state_space_nodes.iter().for_each(|state_space_node| {
+        let point_2dd = state_space_node.get_point_xy();
+        // TODO: show state space node heading
+        context.begin_path();
+        let _result = context.arc(point_2dd.x, point_2dd.y, TANK_RADIUS, 0., TAU);
+        context.stroke();
+      });
+    }
+    context.restore();
+    // end
     Ok(())
   }
 }
@@ -157,10 +179,6 @@ impl Painter for TankPainter {
     let tanks: Ref<VecDeque<Rc<RefCell<dyn Tank>>>> = self.tanks.borrow();
     for tank in tanks.iter() {
       let tank: Ref<dyn Tank> = tank.borrow();
-      if !tank.is_active() {
-        // TODO: remove inactive tanks
-        continue;
-      }
       let _result = self.paint_tank(&*tank);
     }
   }
