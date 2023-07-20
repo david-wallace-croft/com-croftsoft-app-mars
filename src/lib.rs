874bd5d3c443e3dd4cb765cf03225a31e6378ee3
David Wallace Croft <david@CroftSoft.com>
2023-07-20 07:44:53 -0500

    removed wee_alloc

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //! - Copyright: &copy; 2023 [`CroftSoft Inc`]
 //! - Author: [`David Wallace Croft`]
 //! - Created: 2023-03-11
-//! - Updated: 2023-07-02
+//! - Updated: 2023-07-20
 //!
 //! [`CroftSoft Inc`]: https://www.croftsoft.com/
 //! [`David Wallace Croft`]: https://www.croftsoft.com/people/david/
@@ -15,7 +15,6 @@ use com_croftsoft_lib_animation::web_sys::log;
 use constant::INFO;
 use looper::Looper;
 use wasm_bindgen::prelude::*;
-use wee_alloc::WeeAlloc;
 
 pub mod ai;
 pub mod component;
@@ -34,10 +33,6 @@ pub mod root;
 pub mod updater;
 pub mod visitor;
 pub mod world;
-
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
